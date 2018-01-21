c2fcc8109ddad17b2a4bcc9bf6cc221a31f90c99
Gabe Harris <35112663+gabeh73@users.noreply.github.com>
2018-01-21 00:09:31 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+#This function makes the LIST that contains a matrix....
+#Warning, it does nto create a regular matrix.
+#The list created has to be this special format
+#the cache solve function will NOT solve for a normal matrix
+# a normal matrix is not accepeted to cachesolve
+#you will also not be able to run Cache solve the first time without 
+#running this function first
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = numeric()) {
 
-makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+#The function returns a matrix but it first has to check the list to
+#see if a solution has already been cached
+
+cacheSolve <- function(x,...) {
+       
+   m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data,...)
+        x$setinverse(m)
+        m
+
+
 }
